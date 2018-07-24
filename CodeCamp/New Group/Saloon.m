59704acf73018adda34e9be640ea8406e31b9041
Codecamp <Codecamp@mac-mini-7.local>
2018-07-24 10:20:27 +0200

    added data model

---
+++ CodeCamp/New Group/Saloon.m
@@ -0,0 +1,13 @@
+//
+//  Saloon.m
+//  CodeCamp
+//
+//  Created by Codecamp on 24.07.18.
+//  Copyright © 2018 Codecamp. All rights reserved.
+//
+
+#import "Saloon.h"
+
+@implementation Saloon
+
+@end
---
+++ CodeCamp/New Group/Store.m
@@ -0,0 +1,13 @@
+//
+//  Store.m
+//  CodeCamp
+//
+//  Created by Codecamp on 24.07.18.
+//  Copyright © 2018 Codecamp. All rights reserved.
+//
+
+#import "Store.h"
+
+@implementation Store
+
+@end
