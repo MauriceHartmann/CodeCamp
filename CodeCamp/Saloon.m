e2c5087c6b8305a167011cd9e056017e9553cb29
Codecamp <Codecamp@mac-mini-7.local>
2018-07-25 13:55:16 +0200

    Fix: not gain any thirst or hunger if fodder or drinks 0

--- CodeCamp/Saloon.m
+++ CodeCamp/Saloon.m
@@ -16,24 +16,24 @@
 {
     NSLog(@"In Saloon");
     
-    //Thirst gain 20 Point
-    if([myShares getIntFromKey:@"thirst"] < 100)
-    {
-        if([myShares getIntFromKey:@"thirst"] >= 80)
-        {
-            int difference = 100 - [myShares getIntFromKey:@"thirst"];
-            [myShares updateKeyBy:@"thirst" :difference];
-        }
-        else
-        {
-            [myShares updateKeyBy:@"thirst" :20];
-        }
-    }
-    
     //drinks reduces 2
-    if([myShares getIntFromKey:@"drinks"] >= 0)
+    if([myShares getIntFromKey:@"drinks"] > 0)
     {
         [myShares updateKeyBy:@"drinks" :-2];
+        
+        //Thirst gain 20 Point
+        if([myShares getIntFromKey:@"thirst"] < 100)
+        {
+            if([myShares getIntFromKey:@"thirst"] >= 80)
+            {
+                int difference = 100 - [myShares getIntFromKey:@"thirst"];
+                [myShares updateKeyBy:@"thirst" :difference];
+            }
+            else
+            {
+                [myShares updateKeyBy:@"thirst" :20];
+            }
+        }
     }
     
     for (NSString *key in [myShares getAllKeys])
