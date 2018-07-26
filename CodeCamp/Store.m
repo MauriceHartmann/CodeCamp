c764915a38db5c9dd05c8cc0b24e3d4032da8546
Codecamp <Codecamp@mac-mini.local>
2018-07-26 00:13:11 -0700

    attempted fix

--- CodeCamp/Store.m
+++ CodeCamp/Store.m
@@ -14,13 +14,7 @@
 +(void) doAction : (Share *) myShares : (PageViewController *) pageView
 {
     
-    table = [[UITableView alloc] init];
-    tableWidth = [UIScreen mainScreen].bounds.size.width *0.9;
-    tableHeight = [UIScreen mainScreen].bounds.size.height*0.8;
-    tablePosX = ([UIScreen mainScreen].bounds.size.width - tableWidth) / 2;
-    tablePosY = ([UIScreen mainScreen].bounds.size.height - tableHeight) / 2;
-    table.frame = CGRectMake(tablePosX, tablePosY, tableWidth, tableHeight);
-    [pageView.view addSubview:table];
+   
     
 }
 
