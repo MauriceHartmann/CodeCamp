46c8f0dd660678f599471c6f1d5e34445a212984
Codecamp <Codecamp@codecamp-mac-mini-2.local>
2018-07-26 11:02:57 +0200

    Store done

---
+++ CodeCamp/StoreViewController.m
@@ -0,0 +1,45 @@
+//
+//  StoreViewController.m
+//  CodeCamp
+//
+//  Created by Codecamp on 26.07.18.
+//  Copyright © 2018 Codecamp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "StoreViewController.h"
+#import "Share.h"
+#import "Creature.h"
+
+@implementation StoreViewController
+
+Share* mySharesInventory;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    mySharesInventory = Share.sharedSingleton;
+    
+    _foodInventory.text = [NSString stringWithFormat:@"In Inventory: %i", [mySharesInventory getIntFromKey:FODDER]];
+    
+    _drinkInventory.text = [NSString stringWithFormat:@"In Inventory: %i", [mySharesInventory getIntFromKey:DRINKS]];
+}
+
+- (IBAction)retrunAction:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)buyDrinkAction:(id)sender
+{
+    [mySharesInventory updateKeyBy:DRINKS :1];
+    _drinkInventory.text = [NSString stringWithFormat:@"In Inventory: %i", [mySharesInventory getIntFromKey:DRINKS]];
+}
+
+- (IBAction)buyFoodAction:(id)sender
+{
+    [mySharesInventory updateKeyBy:FODDER :1];
+    _foodInventory.text = [NSString stringWithFormat:@"In Inventory: %i", [mySharesInventory getIntFromKey:FODDER]];
+}
+
+@end
